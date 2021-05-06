4c49233dd4fe8ee76373aa435af616878e0d9f49
Josanta <mwarishijoseph@gmail.com>
2021-05-06 20:31:18 +0300

    first commit

---
+++ headermaker.py
@@ -0,0 +1,14 @@
+['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+['accept-encoding'] = 'gzip, deflate, br',
+['accept-language'] = 'en-US,en;q=0.9',
+['cache-control'] = 'max-age=0',
+['content-length'] = 2351
+['content-type'] = 'application/x-www-form-urlencoded',
+['cookie'] = '__cfduid=d0db51d8b3dba740701e1f971a6ae3d0b1618742180; cf_chl_2=0a56fd9aab1b40d; my_ps=!j2By+CkVOxRYH+IECxdeVa/78tX8i+GNBHqFrAi+vUW5aED+v51qlqvtRdAgfMiUF3yG1V8QZJ8ssUs=; TS013f31df=015cfddf55f4159a6f5ba006e7fbe1f052ba7df9517951706c171e8c4b4317d89d0436949dede8d37bb8ad4a5363cab94369552d3c44c18d14528178258e9f48e90a96cbf4; cf_chl_prog=x11',
+['origin'] = 'https://www.leetchi.com',
+['referer'] = 'https://www.leetchi.com/fr/User/Authenticate?returnUrl=%2F',
+['sec-fetch-dest'] = 'document',
+['sec-fetch-mode'] = 'navigate',
+['sec-fetch-site'] = 'same-origin',
+['upgrade-insecure-requests'] = 1,
+['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 Edg/89.0.774.77'
---
+++ quote.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+
+# app.config['SECRET_KEY'] = "JSTANTAKENDRICK"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:10854@localhost/quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kdnbbbzuycrvhc:40779367850fc794c4a76c8a3753236ca076a4ea49e3c82e6e4df45c1943e920@ec2-3-217-219-146.compute-1.amazonaws.com:5432/df7b72jlvl44id'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+
+# Create a Form class
+class QuoteForm(FlaskForm):
+    author = StringField("Author Name", validators=[DataRequired()])
+    quote = TextAreaField("Quote message", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class Favquote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
+
+@app.route('/')
+def index():
+    result = Favquote.query.all()
+    last = len(result) - 1
+    return render_template('index.html', result=result, last=last)
+
+
+@app.route('/quotes')
+def quotes():
+    author = None
+    quote = None
+    form = QuoteForm()
+    if form.validate_on_submit():
+        author = form.author.data
+        form.author.data = ''
+        quote = form.quote.data
+        form.quote.data = ''
+    return render_template('quotes.html', form=form)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquote(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
